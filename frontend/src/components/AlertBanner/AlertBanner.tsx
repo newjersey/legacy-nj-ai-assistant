a07589b47424e899b97427a90c93f9a34a7eebd4
Lizzy Zhang <77908367+ezhangy@users.noreply.github.com>
2025-02-26 13:19:54 -0500

    Add simple alert banner (#38)
    Co-authored-by: sanni <43077388+sannidhishukla@users.noreply.github.com>
    Co-authored-by: Sannidhi Shukla <sannidhi@innovation.nj.gov>

---
+++ frontend/src/components/AlertBanner/AlertBanner.tsx
@@ -0,0 +1,21 @@
+import styles from "./AlertBanner.module.css";
+
+interface Props {
+  messageHtml: string;
+}
+
+export const AlertBanner = (props: Props) => {
+  return (
+    <div
+      data-testid="alert-banner"
+      className="usa-alert usa-alert--info usa-alert--slim usa-alert--no-icon"
+    >
+      <div className="usa-alert__body">
+        <p
+          className={`${styles.alertBannerText} usa-alert__text`}
+          dangerouslySetInnerHTML={{ __html: props.messageHtml }}
+        />
+      </div>
+    </div>
+  );
+};
