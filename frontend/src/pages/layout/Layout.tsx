c4a67d97bbea23b09286457de09463eb55cb1545
elizabeth zhang <elizabeth.zhangey@gmail.com>
2025-02-26 12:39:08 -0500

    reduce font size + replace test AlertBanner with real one

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -53,7 +53,7 @@ const Layout = () => {
     }
   }, [copyClicked]);
 
-  useEffect(() => {}, [appStateContext?.state.isCosmosDBAvailable.status]);
+  useEffect(() => { }, [appStateContext?.state.isCosmosDBAvailable.status]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -77,14 +77,13 @@ const Layout = () => {
   return (
     <div className={styles.layout}>
       <header className={styles.header} role={"banner"}>
-        {/* {ui?.alert_banner_message !== undefined
+        {ui?.alert_banner_message !== undefined
           && ui.alert_banner_message !== null
           && ui.alert_banner_message !== ""
           && (
             <AlertBanner messageHtml={ui.alert_banner_message} />
           )
-        } */}
-        <AlertBanner messageHtml="this is a test message i'm making it long so. it overflows this is a test message i'm making it long so it overflows this is a test message i'm making it long so it o.verflows this is a test message i'm making it long so it overflowsthis is a test message i'm making it long so. it overflows this is a test message i'm making it long so it overflows this is a test message i'm making it long so it o.verflows this is a test message i'm making it long so it overflows" />
+        }
         <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
           <Stack horizontal verticalAlign="center">
             <img src={logo} className={styles.headerIcon} aria-hidden="true" alt="" />
