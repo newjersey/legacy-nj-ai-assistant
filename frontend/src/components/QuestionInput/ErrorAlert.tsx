bf78bce5a5b4c4a74bbd301600f557e5b77f8089
Sannidhi Shukla <sannidhi@innovation.nj.gov>
2025-03-19 12:09:37 -0600

    update button styling

--- frontend/src/components/QuestionInput/ErrorAlert.tsx
+++ frontend/src/components/QuestionInput/ErrorAlert.tsx
@@ -13,16 +13,16 @@ export const ErrorAlert = ({ onRemove, message, alertType }: ErrorAlertProps) =>
 
   return (
     <div
-      className={`usa-alert usa-alert--error usa-alert--slim margin-top-0 line-height-sans-5 width-full padding-y-0 position-relative display-flex flex-justify ${styles.errorAlert}`}
+      className={`usa-alert usa-alert--error usa-alert--slim margin-top-0 line-height-sans-5 width-full padding-y-0 position-relative display-flex flex-align-center flex-justify ${styles.errorAlert}`}
       data-testid={`${alertType}`}
     >
-      <div className="usa-alert__body">
+      <div className={`usa-alert__body ${styles.errorAlertText}`}>
         <p role="alert" className="usa-alert__text maxw-none" id={alertType}>
           {message}
         </p>
       </div>
       <button
-        className={`usa-button usa-button--unstyled margin-right-1 ${styles.closeButton}`}
+        className={`usa-button usa-button--unstyled height-auto margin-right-1 ${styles.closeButton}`}
         aria-label={`Close error alert: ${formattedAlertType}`}
         onClick={() => {
           onRemove(alertType);
